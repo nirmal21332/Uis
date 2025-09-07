736e4f02a09c94170f6e5e79241a6584816506a8
nirmal21332 <nirmal93514@gmail.com>
2025-09-07 08:55:07 +0530

    first commit

--- lib/UI 2/veg_lists.dart
+++
@@ -1,9 +0,0 @@
-import 'Top_bar.dart';
-
-List<VegDesign> vegList = [
-  VegDesign(title: 'Carrot', image: 'https://freepngimg.com/thumb/carrot/9-2-carrot-download-png.png', price: '12.32'),
-  VegDesign(title: 'Spinach', image: 'https://www.pngarts.com/files/6/Green-Spinach-PNG-Background-Image.png', price: '8.78'),
-  VegDesign(title: 'Brinjal', image: 'https://www.pngplay.com/wp-content/uploads/10/Brinjal-Transparent-Images.png', price: '15.65'),
-  VegDesign(title: 'Tomato', image: 'https://freepngimg.com/download/tomato/159306-fresh-photos-tomatoes-bunch-free-photo.png', price: '32.32'),
-  VegDesign(title: 'Brocoli', image: 'https://pngimg.com/uploads/broccoli/broccoli_PNG72970.png', price: '32.22'),
-];
